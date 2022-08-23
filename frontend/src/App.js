12b53ae1e0975549018d7d8f05faebb0d56e92fa
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-23 22:37:01 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from "./pages/Login"
 function App() {
   return (
     <div className="App">
-      
+      <Login></Login>
     </div>
   );
 }
