7508f91d09ce4f16a80fbf5f3db4065446e3afaf
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-25 12:59:51 +0530

    ch

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from ".././components/homepage/Footer";
+// import Navbar from "../../../../k_fw16_583/Navbar";
 
 export const HomePage = () => {
   return (
     <div>
+      {/* <Navbar /> */}
       <Footer />
     </div>
   );
