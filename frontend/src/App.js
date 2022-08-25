442d5188aae0741a3b2491cd574e2b82c75830c7
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-08-25 19:37:44 +0530

    ch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+
+// i have written the below code just for understanding purpose of redux
+// change according to the need
 function App() {
+  const navigate = useNavigate();
+  const { isAuth } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [navigate, isAuth]);
+
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequiredAuth>
+              <Home />
+            </RequiredAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
