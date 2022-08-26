0dc3353874505789969daee07f5a06068278d007
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-26 22:24:56 +0530

    1

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HomePage } from './pages/HomePage'
+
+const App = () => {
+  return (
+    <div>
+        <HomePage></HomePage>
+        <Login></Login>
+    </div>
+  )
+}
+
+export default App
