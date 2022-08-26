fa9589fc6ce56e4c11d7bbaac7fa16bfcddd504b
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-08-26 09:10:54 +0530

    homepage

---
+++ frontend/src/components/homepage/Cards.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Cards = () => {
+  return (
+ <Box m='2rem' w={'250px'} border={'1px'} borderColor='gray.500'>
+
+     </Box>
+  )
+}
+
+export default Cards
