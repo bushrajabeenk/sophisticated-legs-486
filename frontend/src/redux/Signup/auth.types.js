9522320b885d32689d53940c5d6d6ba4fdfd7d83
sumeet.rana <sumeet.rana@innovaccer.com>
2022-09-23 22:18:26 +0530

    feat/empty code

--- frontend/src/redux/Signup/auth.types.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_LOADING = "auth/login/loading";
-export const LOGIN_SUCCESS = "auth/login/success";
-export const LOGIN_ERROR = "auth/login/error";
-export const LOGOUT = "auth/logout";
